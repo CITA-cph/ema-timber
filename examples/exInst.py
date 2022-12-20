338c2e7b99b28a67f1e8b7a3c2c8b5965db86575
Tom Svilans <email@example.com>
2022-12-20 14:38:44 +0100

    feat(examples): added init_examples.py

---
+++ examples/exInst.py
@@ -0,0 +1,52 @@
+import init_examples
+
+import sys
+import os
+import threading
+import socket
+
+from ema_timber.communicate import Client, Server, Instructor, i_sProt
+from ema_timber.communicate.Broadcast import tunein
+
+def main():
+
+    #+++++++++++++++++++++++++++++#
+    ID = "99"
+    HOSTNAME = socket.gethostname()
+    IP = socket.gethostbyname(HOSTNAME)
+    PORT = 52344 #RECIVING DATA AT
+    
+    BROADPORT = 55555 #BROADCASTING 
+
+    print (f"\nCLIENT SERVER : \n\t{HOSTNAME} - {IP}")
+    print (f"PORTS : \n\tReceiving - {ID} : {PORT}\n")
+    #+++++++++++++++++++++++++++++#
+
+    i_s = i_sProt.getprgls()
+
+    threads = []
+
+
+    T_HOST, T_PORT = tunein(BROADPORT, 0)
+    print ("SERVER addr : ", T_HOST, T_PORT)
+    
+    Client.PING(T_HOST, T_PORT, ID, IP, PORT)
+
+    S = threading.Thread(
+        target = Server,
+        args = (IP, PORT, ID, i_s)
+        )
+    S.start()
+    threads.append(S)
+
+    operate = threading.Thread(
+        target = Instructor,
+        args= ( T_HOST, T_PORT, ID, i_s)
+        )
+    operate.start()
+    threads.append(operate)
+
+    for tt in threads:
+        tt.join()
+if __name__ == "__main__":
+    main()
---
+++ examples/init_examples.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+'''
+Add the ema_timber folder to system path
+so that we can load ema_timber from our
+example scripts.
+'''
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+base_dir = os.path.dirname(this_dir)
+tmp_dir = os.path.join(base_dir, "tmp")
+
+sys.path.append(os.path.join(base_dir, "src", "python"))
+
+import ema_timber
