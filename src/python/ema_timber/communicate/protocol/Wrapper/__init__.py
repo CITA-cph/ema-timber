a1a004df219b125a85c084262f9d120016dabdb8
Tom Svilans <email@example.com>
2023-01-05 17:34:54 +0100

    fix(wrapper): removed sys.path addition

--- src/python/ema_timber/communicate/protocol/Wrapper/__init__.py
+++ src/python/ema_timber/communicate/protocol/Wrapper/__init__.py
@@ -5,7 +5,7 @@ current = os.path.dirname(os.path.abspath(__file__))
 #print (current)
 parent = os.path.abspath(os.path.join(current ,".."))
 #print(parent)
-sys.path.append(parent)
+#sys.path.append(parent)
 
 from core import Client
 from core import Package
