fbad5341f246d964ff744f4e836a86582bac192f
Tom Svilans <email@example.com>
2023-01-06 10:19:51 +0100

    fix(build): separated build and install

--- src/python/build.bat
+++ src/python/build.bat
@@ -1,2 +1,1 @@
 python -m build --wheel
-pip install --upgrade --force-reinstall dist/ema_timber-0.1-py3-none-any.whl
---
+++ src/python/install.bat
@@ -0,0 +1,1 @@
+pip install --upgrade --force-reinstall dist/ema_timber-0.1-py3-none-any.whl
